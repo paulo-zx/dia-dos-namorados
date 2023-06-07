48c337f94f687482e8a774fedbda2ecd6d661f88
paulo-zx <paulo-zx@hotmail.com>
2023-06-07 14:06:07 -0300

    dia dos namorados

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const nao = document.getElementById("#nao");
+
+function fuja(){
+    var botaoNao = document.getElementById("nao")
+
+    var larguraJanela = window.innerWidth;
+    var alturaJanela = window.innerHeight;
+
+    var maxX = larguraJanela - botaoNao.offsetWidth;
+    var maxY = alturaJanela - botaoNao.offsetHeight;
+
+    var aleatorioX = Math.floor(Math.random() * maxX);
+    var aleatorioY = Math.floor(Math.random() * maxY);
+
+    botaoNao.style.left = aleatorioX + "px";
+    botaoNao.style.top = aleatorioY + "px";
+
+
+}
